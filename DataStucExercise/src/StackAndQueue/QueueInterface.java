b6845d894576fc07387f715b45987eb005ef08f4
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-02-09 22:05:57 +0700

    [ADD] Stack and Queue Interface

---
+++ DataStucExercise/src/StackAndQueue/QueueInterface.java
@@ -0,0 +1,17 @@
+/*
+ * Created By Safe_IT23
+ */
+package StackAndQueue;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public interface QueueInterface {
+    public void enqueue(Object e);
+    public Object dequeu();
+    public int size();
+    public boolean isEmpty();
+    
+    
+}
---
+++ DataStucExercise/src/StackAndQueue/StackInterface.java
@@ -0,0 +1,16 @@
+/*
+ * Created By Safe_IT23
+ */
+package StackAndQueue;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public interface StackInterface {
+    public void push(Object e);
+    public Object pop();
+    public int size();
+    public boolean isEmpty();
+    
+}
