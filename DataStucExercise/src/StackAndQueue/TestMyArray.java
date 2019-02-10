9a4e1547e61864b5abcfbfe66e0e5b7736982013
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-02-10 14:02:37 +0700

    [ADD] TestMyArray

---
+++ DataStucExercise/src/StackAndQueue/TestMyArray.java
@@ -0,0 +1,15 @@
+/*
+ * Created By Safe_IT23
+ */
+package StackAndQueue;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class TestMyArray {
+    public static void main(String[] args) {
+        MyArray<String> array = new MyArray(5);
+        
+    }
+}
