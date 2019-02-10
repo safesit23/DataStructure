49c570f55e12128edd90294851f54991d3779ae6
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-02-10 13:00:17 +0700

    [ADD] MyArray

---
+++ DataStucExercise/src/StackAndQueue/MyArray.java
@@ -0,0 +1,44 @@
+/*
+ * Created By Safe_IT23
+ */
+package StackAndQueue;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class MyArray implements QueueInterface<String>,StackInterface<String>{
+
+    @Override
+    public void enqueue(String e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String dequeue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void push(String e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String pop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
