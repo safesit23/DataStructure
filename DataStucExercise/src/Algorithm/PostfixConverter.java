5716c7d58e93bf43c406e4d3997474611c9ed74c
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-02-18 22:02:37 +0700

    [UPDATE] add method

--- DataStucExercise/src/Algorithm/PostfixConverter.java
+++ DataStucExercise/src/Algorithm/PostfixConverter.java
@@ -8,5 +8,21 @@ package Algorithm;
  * @author jatawatsafe
  */
 public class PostfixConverter {
+    public static String postfixConverter(){
+        return null;
+    }
     
+    public boolean isOperator(char t){
+        switch(t){
+            case '+' :
+            case '-' :
+            case '*' :
+            case '/' : return true;
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
 }
