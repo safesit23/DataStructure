3dac959dc25a6348a2efd408a6ad23887e41c6d4
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-02-20 00:02:30 +0700

    [UPDATE] Test Postfix by SAFE

--- DataStucExercise/test/PostfixTest.java
+++ DataStucExercise/test/PostfixTest.java
@@ -1,6 +1,5 @@
 
 import Algorithm.PostfixConverter;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -13,14 +12,45 @@ import org.junit.Test;
  * @author jatawatsafe
  */
 public class PostfixTest {
-    private String equation1 = "a+b*c";
+    private String equation1 = "x-y*z";
+    private String equation2 = "x*y-z";
+    private String equation3 = "a+b*c/d-e";
+    private String equation4 = "x*(y+z)";
+    private String equation5 = "x*(y+z–(a/b+c)*d)/e";
 
     @Test
     public void test1() {
-        String expectResult = "abc*+";
+        String expectResult = "xyz*-";
         String result = PostfixConverter.postfixConverter(equation1);
         assertEquals(expectResult, result);
     }
     
+    @Test
+    public void test2() {
+        String expectResult = "xy*z-";
+        String result = PostfixConverter.postfixConverter(equation2);
+        assertEquals(expectResult, result);
+    }
+    
+    @Test
+    public void test3() {
+        String expectResult = "abc*d/+e-";
+        String result = PostfixConverter.postfixConverter(equation3);
+        assertEquals(expectResult, result);
+    }
+    
+//    @Test
+//    public void test4Level2() {
+//        String expectResult = "xyz+*";
+//        String result = PostfixConverter.postfixConverter(equation4);
+//        assertEquals(expectResult, result);
+//    }
+    
+//    @Test
+//    public void test5Level3() {
+//        String expectResult = "";
+//        String result = PostfixConverter.postfixConverter(equation5);
+//        assertEquals(expectResult, result);
+//    }
     
 }
