9bfa166a6d834d4a9691dc40761017291768b841
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-02-22 12:27:06 +0700

    [UPDATE] add Student Class

--- DataStucExercise/src/Tree/TestBSTbySet.java
+++ DataStucExercise/src/Tree/TestBSTbySet.java
@@ -3,6 +3,7 @@
  */
 package Tree;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -12,17 +13,60 @@ import java.util.TreeSet;
  */
 public class TestBSTbySet {
     public static void main(String[] args) {
-        Set<String> x = new TreeSet();
-        x.add("A");
-        x.add("B");
-        x.add("C");
-        x.add("D");
-        x.add("E");
-        x.add("F");
-        x.add("G");
-        x.add("H");
+        Set<Student> x = new TreeSet();
+        x.add(new Student(1, "A"));
+        x.add(new Student(2, "B"));
+        x.add(new Student(3, "C"));
+        x.add(new Student(4, "D"));
+        x.add(new Student(5, "E"));
+        x.add(new Student(5, "F"));
         //Display
         System.out.println(x);
-        System.out.printf("Has G : %b\n",x.contains("G"));
+        Student std1 = new Student(3, "xxxx");
+        System.out.printf("Has 3 : %b\n",x.contains(std1));
     }
+}
+
+class Student implements Comparable<Student>, Comparator<Student>{
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + '}';
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        //Use - because if use = it will replace the id
+        return this.id - student.getId();
+    }
+
+    @Override
+    public int compare(Student s1, Student s2) {
+        return s1.getName().compareTo(s2.getName());
+    }
+    
+    
 }
