0885fe021f868bc60206dcadfb204bceb0bafad6
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-03-01 10:41:49 +0700

    [UPDATE] test hashcode

--- DataStucExercise/src/HashTable/TestHashSet.java
+++ DataStucExercise/src/HashTable/TestHashSet.java
@@ -12,6 +12,18 @@ import java.util.Set;
  */
 public class TestHashSet {
     public static void main(String[] args) {
+        Set<String> s = new HashSet<>();
+        s.add("Cat");
+        s.add("Dog");
+        s.add("Bird");
+        s.add("Duck");
+        //Display hashcode1
+        System.out.println("Code: "+"Cat".hashCode());
+        //Display hashcode2
+        String x = new String("Cat");
+        System.out.println("Code2: "+x.hashCode());
+        //The same object must be same hashcode because it use to find
         
+        System.out.println("Is set contain cat ? : "+s.contains("Cat"));
     }
 }
