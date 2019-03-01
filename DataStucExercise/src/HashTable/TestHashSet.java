cf9830e4205d25e76c9784c5d79402af0b58b71d
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-03-01 10:54:26 +0700

    [UPDATE] add testHashCode with NewStudent

--- DataStucExercise/src/HashTable/TestHashSet.java
+++ DataStucExercise/src/HashTable/TestHashSet.java
@@ -12,6 +12,23 @@ import java.util.Set;
  */
 public class TestHashSet {
     public static void main(String[] args) {
+        //testHashCode1();
+        testHashCode2();
+    }
+    
+    static void testHashCode2(){
+        Set<NewStudent> st = new HashSet<>();
+        st.add(new NewStudent(1001, "Somchai"));
+        st.add(new NewStudent(1002, "Somsri"));
+        st.add(new NewStudent(1003, "Somkiet"));
+        NewStudent ns = new NewStudent(1002, "Somsri");
+        NewStudent ns2 = new NewStudent(1002, "Somsri");
+        System.out.println("ns: "+ns.hashCode());
+        System.out.println("ns2: "+ns2.hashCode());
+        System.out.println("Is st contains ns? :"+st.contains(ns));
+    }
+    
+    static void testHashCode1(){
         Set<String> s = new HashSet<>();
         s.add("Cat");
         s.add("Dog");
@@ -26,4 +43,37 @@ public class TestHashSet {
         
         System.out.println("Is set contain cat ? : "+s.contains("Cat"));
     }
+}
+
+class NewStudent{
+    private int id;
+    private String name;
+
+    public NewStudent(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + '}';
+    }
+    
+    
 }
