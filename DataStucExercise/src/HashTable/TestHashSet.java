402b7c42de73bc04a87aff7e6717c89e4b45131c
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-03-01 10:35:34 +0700

    [ADD] TestHashSet

---
+++ DataStucExercise/src/HashTable/TestHashSet.java
@@ -0,0 +1,17 @@
+/*
+ * Created By Safe_IT23
+ */
+package HashTable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class TestHashSet {
+    public static void main(String[] args) {
+        
+    }
+}
