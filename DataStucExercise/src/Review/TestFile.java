faa8c158e0e44e703736354a20e657c5060eaf06
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-03-06 14:54:46 +0700

    [UPDATE] add testScanner

--- DataStucExercise/src/Review/TestFile.java
+++ DataStucExercise/src/Review/TestFile.java
@@ -5,7 +5,9 @@ package Review;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,6 +29,25 @@ public class TestFile {
             int num = sc.nextInt();
             System.out.println(num);
         } catch (FileNotFoundException ex) {
+            Logger.getLogger(TestFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void testScanner(){
+        ArrayList<String> listAll = new ArrayList();  //เพิ่ม ArrayList
+        try{
+            File myFile=new File("StudentList.txt");
+            Scanner sc = new Scanner(myFile);
+            while(sc.hasNextLine()){   //Loop นับบรรทัด   โดยผูกกับไฟล์
+                String line=sc.nextLine();
+                //System.out.println(line);
+                StringTokenizer stk = new StringTokenizer(line, ". ",false);
+                while(stk.hasMoreElements()){         //Loop นับคำ โดยผูกกับString
+                    String text=stk.nextToken();
+                    //System.out.println(scStr.next());
+                }
+            }
+        }catch (FileNotFoundException ex) {
             Logger.getLogger(TestFile.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
