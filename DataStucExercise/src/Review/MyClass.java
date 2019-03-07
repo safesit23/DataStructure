2b6a5d5de484fff2c1a8b50a2da821a0381c1610
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-03-07 19:33:26 +0700

    [ADD] How to write inner class

---
+++ DataStucExercise/src/Review/MyClass.java
@@ -0,0 +1,18 @@
+/*
+ * Created By Safe_IT23
+ */
+package Review;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class MyClass {
+    private int number;
+    private InnerMyClass inner = new InnerMyClass();
+    
+    private static class InnerMyClass{
+        
+    }
+}
+
