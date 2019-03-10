aa85e2ff45b70c40d91701c1b293e46ce46f3297
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-03-10 14:08:20 +0700

    [ADD] TestMostNumber

---
+++ DataStucExercise/src/Algorithm/FindMode/TestMostNumber.java
@@ -0,0 +1,16 @@
+/*
+ * Created By Safe_IT23
+ */
+package Algorithm.FindMode;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class TestMostNumber {
+    public static void main(String[] args) {
+        int[] numbers = {29,34,53,74,85,96,36,36,25,45,67,96};
+        String result = FindMode.findMode(numbers);
+        System.out.println(result);
+    }
+}
