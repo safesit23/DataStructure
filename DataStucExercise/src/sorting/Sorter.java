025e24c5bbc2ee1363f1b4fb938c8da665651dde
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-04-26 12:18:06 +0700

    [ADD] Sorting

---
+++ DataStucExercise/src/sorting/Sorter.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sorting;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class Sorter {
+
+    public static void insertion(Object[] data) {
+        System.out.println("------SORT BY INSERTION-----");
+        insertionUsingComparable(data);
+    }
+
+    public static void insertion(Object[] data, Comparator c) {
+        System.out.println("------SORT BY INSERTION-----");
+        if (c == null) {
+            insertionUsingComparable(data);
+        } else {
+            insertionUsingComparator(data, c);
+        }
+    }
+
+    public static void insertionUsingComparable(Object[] data) {
+        for (int i = 1; i < data.length; i++) {
+            Comparable tmp = (Comparable) data[i];
+            int j = i;
+            System.out.println(i + ".)" + display(data));
+            message(data[i], data[j - 1]); //display process
+            for (; j > 0 && tmp.compareTo((Comparable) data[j - 1]) < 0; j--) { //when temp is more than will break loop
+                System.out.println("      MOVE");
+                data[j] = data[j - 1];
+                System.out.println("      Data: " + display(data));
+                message(data[i], data[j - 1]); //display process
+            }
+            System.out.println("      STOP");
+            data[j] = tmp;
+            System.out.println("--->>AfterReplace: " + display(data) + "\n");
+        }
+    }
+
+    public static void insertionUsingComparator(Object[] data, Comparator c) {
+        for (int i = 1; i < data.length; i++) {
+            Object tmp = (Comparable) data[i];
+            int j;
+            for (j = i; j > 0 && c.compare(tmp, data[j - 1]) < 0; j--) {
+                data[j] = data[j - 1];
+            }
+            data[j] = tmp;
+        }
+    }
+
+    public static void selectionSort(Comparable x[]) {
+        System.out.println("------SORT BY SELECTION-----");
+        for (int i = x.length - 1; i > 0; i--) {
+            int max = 0;
+            for (int j = 1; j <= i; j++) {
+                if (x[j].compareTo(x[max]) > 0) {
+                    max = j;
+                }
+            }
+            if (max != i) {
+                swap(x,max, i);
+            }
+        }
+    }
+    
+    public static void swap(Object x[], int i, int j){
+        Object tmp = x[i];
+        x[i] = x[j];
+        x[j] = tmp;
+    }
+    
+
+    public static String display(Object[] data) {
+        String display = "";
+        for (Object i : data) {
+            display = display + i + " ";
+        }
+        return display;
+    }
+
+    public static void message(Object i, Object j) {
+        Comparable tmp = (Comparable) i;
+        Comparable dataJ = (Comparable) j;
+        int compare = tmp.compareTo(dataJ);
+        System.out.printf("----->COMPARE : %s - %s = %d",tmp,dataJ,compare);
+        //System.out.println("----->COMPARE tmp: " + i + " with j: " + j  + "  get  " + compare);
+    }
+}
---
+++ DataStucExercise/src/sorting/Tester.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sorting;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class Tester {
+    public static void main(String[] args) {
+        test2();
+    }
+    
+    public static void printArray(Object[] x) {
+        for (int i = 0; i < x.length; i++) {
+            System.out.printf("%d%s",x[i],(i+1)%10==0?"\n":"\t");
+        }
+        System.out.println("");
+    }
+    
+    public static void test1(){
+        Integer[]x= new Integer [10];
+        Random r = new Random();
+        for (int i = 0; i < x.length; i++) {
+            x[i] = r.nextInt(100);
+        }
+        System.out.println("Original order: ");
+        printArray(x);
+        //------------CHOOSE SORT---------------
+        //Insertion Type1
+        Sorter.insertion(x);
+        
+        //Insertion Type2
+        /*
+        Sorter.insertion(x,new Comparator<Integer>(){   //Decreasing
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+            
+        });
+        */
+        
+        //Selection Sort
+        //Sorter.selectionSort(x);
+        
+        
+        System.out.println("After Sorted: ");
+        printArray(x);
+        
+        
+    }
+    
+    public static void test2(){
+        Integer[] x = new Integer[200_000];
+        Integer[] y = new Integer[200_000];
+        Random r = new Random();
+        for (int i = 0; i < x.length; i++) {
+            x[i] = r.nextInt(1000000);
+        }
+        System.arraycopy(x, 0, y, 0, x.length);
+        long before = System.currentTimeMillis();
+        Arrays.parallelSort(x);
+        long duration1 = System.currentTimeMillis()-before;
+        
+        before = System.currentTimeMillis();
+        Sorter.insertion(y);
+        long duration2 = System.currentTimeMillis() - before;
+        System.out.println("Parallel sort : "+duration1+"milliseconds");
+        System.out.println("Onsection sort : "+duration2+"milliseconds");
+        System.out.println("After order");
+        printArray(x);
+    }
+}
+
